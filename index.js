932ed0eb14483ef68a1daea0838f17425c37f20e
MauroPello <mauro.pellonara@outlook.com>
2024-09-03 16:24:34 +0200

    Clean up

--- index.js
+++ index.js
@@ -4,16 +4,13 @@ const topicsClients = {};
 const server = new WebSocketServer({ port: 20000 });
 
 server.on('connection', (client) => {
-    console.log('New connection accepted.');
-
     client.on('message', (rawMessage) => {
-        console.log('Message arrived: \n' + rawMessage);
         const message = JSON.parse(rawMessage);
         const { type, topic } = message;
 
         switch (type) {
             case 'subscribe':
-                console.log(`New subscription\n- topic ${topic}\n`);
+                console.log("New subscription to topic ", topic);
 
                 // if it's a new topic create it
                 if (!topicsClients[topic]) {
@@ -24,7 +21,7 @@ server.on('connection', (client) => {
                 topicsClients[topic].push(client);
 
                 // when client closes connection, remove it from the topic
-                ws.on('close', () => {
+                client.on('close', () => {
                     topicsClients[topic] = topicsClients[topic].filter(c => c !== client);
 
                     // delete the topic if it was the last client connected
@@ -36,12 +33,10 @@ server.on('connection', (client) => {
                 break;
             case 'publish':
                 const payload = JSON.stringify(message.payload);
-                console.log(`Publish msg\n- topic ${topic}\n- payload ${payload}`);
+                console.log(`New message on topic ${topic} with payload: `, payload);
 
                 // send message to all clients subscribed to the topic
-                topicsClients[topic]
-                    .filter(client => client.readyState !== WebSocket.OPEN)
-                    .forEach(client => client.send(payload));
+                topicsClients[topic]?.filter(client => client.readyState !== WebSocket.OPEN).forEach(client => client.send(payload));
                 break;
             default:
                 console.log('Unknown message type');
