2db6ab589048ce6bb838d265121ffee47e170119
MauroPello <mauro.pellonara@outlook.com>
2024-09-03 15:29:25 +0200

    Improve typing

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 import { WebSocketServer, WebSocket } from 'ws';
 
-const topics = {};
-const wss = new WebSocketServer({ port: 20000 });
+const topicsClients = {};
+const server = new WebSocketServer({ port: 20000 });
 
-wss.on('connection', (ws) => {
+server.on('connection', (client) => {
     console.log('New connection accepted.');
 
-    ws.on('message', (rawMessage) => {
+    client.on('message', (rawMessage) => {
         console.log('Message arrived: \n' + rawMessage);
         const message = JSON.parse(rawMessage);
         const { type, topic } = message;
@@ -15,37 +15,33 @@ wss.on('connection', (ws) => {
             case 'subscribe':
                 console.log(`New subscription\n- topic ${topic}\n`);
 
-                // create topic if it wasn't there already
-                if (!topics[topic]) {
-                    topics[topic] = [];
+                // if it's a new topic create it
+                if (!topicsClients[topic]) {
+                    topicsClients[topic] = [];
                 }
 
                 // add client to topic
-                topics[topic].push(ws);
+                topicsClients[topic].push(client);
 
                 // when client closes connection, remove it from the topic
                 ws.on('close', () => {
-                    topics[topic].filter(client => client !== ws);
+                    topicsClients[topic] = topicsClients[topic].filter(c => c !== client);
 
                     // delete the topic if it was the last client connected
-                    if (topics[topic].size === 0) {
-                        delete topics[topic];
+                    if (topicsClients[topic].length === 0) {
+                        delete topicsClients[topic];
                     }
                 });
 
                 break;
             case 'publish':
-                const messagePayload = JSON.stringify(message.msg);
-                console.log(`Publish msg\n- topic ${topic}\n- msg ${messagePayload}`);
+                const payload = JSON.stringify(message.payload);
+                console.log(`Publish msg\n- topic ${topic}\n- payload ${payload}`);
 
-                // send message to all clients subscribed to the topic if it exists
-                if (topics[topic]) {
-                    topics[topic].forEach(client => {
-                        if (client.readyState === WebSocket.OPEN) {
-                            client.send(messagePayload);
-                        }
-                    });
-                }
+                // send message to all clients subscribed to the topic
+                topicsClients[topic]
+                    .filter(client => client.readyState !== WebSocket.OPEN)
+                    .forEach(client => client.send(payload));
                 break;
             default:
                 console.log('Unknown message type');
